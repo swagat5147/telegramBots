979377a1ad658ef476d5c06ae757407ff7b0403d
swagat5147 <swagatparija.1@gmail.com>
2019-01-17 19:10:37 +0530

    added new stuff

--- bot.py
+++ bot.py
@@ -22,12 +22,17 @@ def bop(bot, update):
 	bot.send_photo(chat_id=chat_id, photo = url)
 	print("Sending photo...")
 
+def stfu(bot, update):
+	chat_id = update.message.chat_id
+	bot.send_messsage(chat_id = chat_id, text = "Shut The Fuck Up Subhrajit")
+
 def main():
 	TOKEN = os.getenv("TOKEN")
 	print("Running bot")
 	updater = Updater(TOKEN)
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler('bop', bop))
+	dp.add_handler(CommandHandler('stfu', stfu))
 	updater.start_polling()
 	updater.idle()
 
