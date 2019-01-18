4643b3c38c25f9c8690e2b89a9779833782206b4
swagat5147 <swagatparija.1@gmail.com>
2019-01-18 17:19:06 +0530

    logging 3

--- bot.py
+++ bot.py
@@ -6,8 +6,6 @@ import os
 
 app = Flask(__name__, static_folder="/static")
 
-DATA = "Hello"
-
 def get_url():
 	contents = requests.get('https://random.dog/woof.json').json()
 	image_url = contents['url']
@@ -23,16 +21,16 @@ def get_image_url():
 
 @app.route("/", methods=["GET"])
 def index():
-	print(DATA)
-	return render_template("index.html", data=DATA)
+	return render_template("index.html")
 
 @app.route("/recieve", methods=["GET","POST"])
 def receive():
 	print("Running bot")
-	global DATA
-	DATA=request.data
-	print(DATA)
-	return render_template("index.html", data=DATA)
+	if request.method = 'GET':
+		return jsonify(request.get_json())
+	else:
+		print(request.get_json())
+		return jsonify(request.get_json())
 
 if __name__ == '__main__':
 	TOKEN = os.environ.get('TOKEN')
