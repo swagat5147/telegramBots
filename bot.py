237e86d8419de0a779bdc16b308d03e2ae67fe55
ommmishra <ommmishra830@gmail.com>
2019-01-19 14:33:06 +0530

    Initializing

---
+++ bot.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify, render_template
+from gevent.pywsgi import WSGIServer
+import requests
+import re
+import os
+
+app = Flask(__name__)
+
+TOKEN = os.environ.get('TOKEN')
+BASE_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+@app.route("/", methods = ["GET"])
+def index():
+	return render_template("index.html")
+
+@app.route("/update", methods = ["POST"])
+def update():
+	print("RunningBot.......")
+	print(request.get_json())
+
+if __name__ == 'main':
+	port = int(os.environ.get('PORT', 5000))
+	http_server = WSGIServer(('', port),app)
+	http_server.serve_forever() 
