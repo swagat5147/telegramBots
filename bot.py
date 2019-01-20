49f07b5c36344a480e26aa1ab2e78a5948f47dd8
Omm Mishra <ommmishra830@gmail.com>
2019-01-20 11:46:50 +0530

    Added verification

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 TOKEN = os.environ.get('TOKEN')
 BASE_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+group_chat_id = os.get.environ.get('group_chat_id')
 
 @app.route("/", methods = ["GET"])
 def index():
@@ -19,15 +20,21 @@ def update():
 	print(request.get_json())
 	data = request.get_json()
 
-	if 'left_chat_member' in data['message']:
-		print(data['message']['left_chat_member']['first_name'])
-	
-	if 'new_chat_member' in data['message']:
-		print(data['message']['new_chat_member']['first_name'])
-		name = data['message']['new_chat_member']['first_name']
-		chat_id = data['message']['chat']['id']
-		r = requests.post(BASE_URL+ "sendMessage", data={'chat_id': chat_id, 'text': "Welcome to Codex " + name})
-	
+	if data['message']['chat']['id'] == group_chat_id
+
+		if 'left_chat_member' in data['message']:
+			print(data['message']['left_chat_member']['first_name'])
+			name = data['message']['left_chat_member']['first_name']
+			chat_id = data['message']['chat_id']['id']
+			r = requests.post(BASE_URL+ "sendMessage", data={'chat_id': chat_id, 'text': name + " Left Codex" })
+
+
+		if 'new_chat_member' in data['message']:
+			print(data['message']['new_chat_member']['first_name'])
+			name = data['message']['new_chat_member']['first_name']
+			chat_id = data['message']['chat']['id']
+			r = requests.post(BASE_URL+ "sendMessage", data={'chat_id': chat_id, 'text': "Welcome to Codex " + name})
+		
 	return "200, OK"
 
 if __name__ == '__main__':
