13f789c9bdae949af6509a2d47b7b72c794f94dc
Omm Mishra <ommmishra830@gmail.com>
2019-01-20 13:58:10 +0530

    added xkcd support

--- xkcd.py
+++ xkcd.py
@@ -2,7 +2,7 @@ import requests
 from random import randint
 random = randint(1, 2100)
 i = requests.get("https://xkcd.com/"+str(random)+"/info.0.json")
-s
+
 def start():
 	if i.status_code == 200:
 		image = i.json()
