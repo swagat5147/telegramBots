e6901b6f288abd19899dc1ccb25f7c04b49bb132
Omm Mishra <ommmishra830@gmail.com>
2019-01-20 14:06:10 +0530

    fixed errors

--- xkcd.py
+++ xkcd.py
@@ -2,14 +2,11 @@ import requests
 from random import randint
 random = randint(1, 2100)
 i = requests.get("https://xkcd.com/"+str(random)+"/info.0.json")
-
-def start():
-	if i.status_code == 200:
-		image = i.json()
-		url = image.get("img")
-		text = image.get("alt")
-		return url, text
-	else:
-		return
+if i.status_code == 200:
+	image = i.json()
+	url = image.get("img")
+	text = image.get("alt")
+	 url, text
+	
 
 
