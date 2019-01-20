f195ee03d981861c0f72130271038a81687ef5b4
Omm Mishra <ommmishra830@gmail.com>
2019-01-20 12:13:26 +0530

    fixed errors

--- bot.py
+++ bot.py
@@ -20,9 +20,9 @@ def update():
 	print(request.get_json())
 	data = request.get_json()
 	print(group_chat_id)
-	print(data['message']['chat']['id'])
+	group_data = data['message']['chat']['id']
 
-	if (data['message']['chat']['id'] == group_chat_id):
+	if (group_data == group_chat_id):
 
 		if 'left_chat_member' in data['message']:
 			print(data['message']['left_chat_member']['first_name'])
